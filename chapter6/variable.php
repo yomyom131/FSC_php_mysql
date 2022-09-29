300fa8b810653880e0e47fcd4cfa28506f653b16
Sarah Ree <hoonjang131@gmail.com>
2022-09-29 14:44:30 +0900

    6. variable

---
+++ chapter6/variable.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Variable</h1>
+        <?php
+            $name = "leezch";
+            echo "Lorem ipsum dolor, sit amet consectetur ".$name." adipisicing elit. Iure, blanditiis tempora sunt, sit ".$name." quas odit fugiat quidem exercitationem ".$name." voluptatem veniam mollitia neque ".$name." eius et? Ipsa quis officia eum reiciendis assumenda!".$name;
+        ?>
+    </body>
+</html>
