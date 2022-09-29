a09b8a28069b74545c2961fb408389561c4496c8
Sarah Ree <hoonjang131@gmail.com>
2022-09-29 14:43:41 +0900

    5. dataType

---
+++ chapter5/string.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>String & String Operator</h1>
+        <?php
+            echo 'Hello World';
+        ?>
+        <h2>concatenation operator</h2>
+        <?php
+            echo "Hello "."World";
+        ?>
+        <h2>String length function</h2>
+        <?php
+            echo stlen("Hello World");
+        ?>
+    </body>
+</html>
