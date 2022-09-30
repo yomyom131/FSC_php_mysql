7adfd96e99ffd32fdf180a39f674e4eeb26892c8
Sarah Ree <hoonjang131@gmail.com>
2022-09-30 16:16:04 +0900

    11. boolean

---
+++ chapter11/comparison.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Comparison Operators & Boolean data type</h1>
+        <?php
+            var_dump(11);
+        ?>
+        <br>
+        <?php
+            var_dump('11');
+        ?>
+        <br>
+        <?php
+            var_dump(1+1);
+        ?>
+        <h2>1==1</h2>
+        <?php
+            var_dump(1==1);
+        ?>
+        <h2>1>1</h2>
+        <?php
+            var_dump(1>1);
+        ?>
+        <h2>1>=1</h2>
+        <?php
+            var_dump(1>=1);
+        ?>
+        <h2>0==null</h2>
+        <?php
+            var_dump(0==null);
+        ?>
+        <h2>0===null</h2>
+        <?php
+            var_dump(0===null);
+        ?>
+        <h2>1==1.0</h2>
+        <?php
+            var_dump(1==1.0);
+        ?>
+        <h2>1>='1'</h2>
+        <?php
+            var_dump(1>='1');
+        ?>
+        <h2>9==true</h2>
+        <?php
+            var_dump(9==true);
+        ?>
+    </body>
+</html>
---
+++ chapter11/conditional.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Conditional</h1>
+        <?php
+            echo '1<br>';
+            if(true) {
+                echo '2-1<br>';
+            } else {
+                echo '2-2<br>';
+            }
+            echo '3<br>';
+        ?>
+    </body>
+</html>
