8278582f05dc033d8b5bdae35b7d1794e7f3122c
Sarah Ree <hoonjang131@gmail.com>
2022-10-04 15:06:44 +0900

    20. C/D/U

--- chapter20/lib/print.php
+++ chapter20/lib/print.php
@@ -28,31 +28,3 @@
         }
     }
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>
-            <?php
-                print_title();
-            ?>
-        </title>
-    </head>
-    <body>
-        <h1><a href="index5.php">WEB</a></h1>
-        <ol>
-            <?php
-                print_list();
-            ?>
-        </ol>
-        <h2>
-            <?php
-                print_title();
-            ?>
-        </h2>
-        <?php
-            print_description();
-        ?>
-    </body>
-</html>
---
+++ chapter20/view/top.php
@@ -0,0 +1,21 @@
+<?php
+    require_once('lib/print.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>
+            <?php
+                print_title();
+            ?>
+        </title>
+    </head>
+    <body>
+        <h1><a href="index5.php">WEB</a></h1>
+        <ol>
+            <?php
+                print_list();
+            ?>
+        </ol>
